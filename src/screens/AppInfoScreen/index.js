878339209979436356485bab862b3d192e6d608e
felipeseolin <felipe.seolin@hotmail.com>
2019-12-03 17:55:12 -0300

    More info about.

--- src/screens/AppInfoScreen/index.js
+++ src/screens/AppInfoScreen/index.js
@@ -18,7 +18,7 @@ const AppInfoScreen = () => (
       </Info>
       <Info title="Como funciona?">
         <Text>
-          Ao acessar o app já é possível visualizar todaas as caravanas
+          Ao acessar o app já é possível visualizar todas as caravanas
           cadastradas, de forma que ao fazer uma conta no app, é possível
           cadastrar um caravana sua e também ingressar em outras caravanas.
         </Text>
@@ -26,7 +26,22 @@ const AppInfoScreen = () => (
       <Info title="Como posso fazer uma conta?">
         <Text>
           Para fazer uma conta é muito simples! Basta adicionar o seu e-mail
-          e uma senha de no mínimo seis caracteres.
+          e uma senha de no mínimo seis caracteres. Para isto, faça o login
+          e crie uma conta também.
+        </Text>
+      </Info>
+      <Info title="Quem pode visualizar as caravanas cadastradas?">
+        <Text>
+          Qualquer um pode visualizar as caravanas cadastradas no sistema,
+          de modo que qualquer um que esteja logado possa também ingressar
+          em uma caravana.
+        </Text>
+      </Info>
+      <Info title="Como posso ver quem ingressou em uma caravana?">
+        <Text>
+          Através da aba Usuário é possível visualizar as suas caravanas,
+          após isto basta selecionar. Nesta tela, também é possível
+          editar as informações da caravana e até mesmo exclui-la.
         </Text>
       </Info>
     </View>
